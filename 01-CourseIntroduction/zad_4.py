ee7d33e65b9b47a6f3a2bc6da87fe5cdcf94a052
Nikola Deja <nikolaa.deja@gmail.com>
2023-10-06 18:11:14 +0200

    zadanie domowe

---
+++ 01-CourseIntroduction/zad_4.py
@@ -0,0 +1,4 @@
+width=17
+hight=12.0  
+
+print(width//2, width//2.0, hight/3, sep='\n')
---
+++ 01-CourseIntroduction/zad_5.py
@@ -0,0 +1,4 @@
+cel=float(input("What's the temperature in Celcius? "))
+far=round((1.8*cel)+32 ,2)
+
+print("That's", far,"in Fahrenheit")
